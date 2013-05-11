f9b63d74936c9eb112c610965658708cec92cc11
cathalcleary <cathalcleary@gmail.com>
2013-05-11 08:15:52 -0500

    Added username and password to REST controller methods

--- ccms-app/src/main/java/ie/cit/adf/ccms/web/CCMSRestController.java
+++ ccms-app/src/main/java/ie/cit/adf/ccms/web/CCMSRestController.java
@@ -6,12 +6,16 @@ import ie.cit.adf.ccms.service.CMSService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
 
 @Controller
 @RequestMapping("api")
@@ -20,29 +24,34 @@ public class CCMSRestController {
 	@Autowired
 	private CMSService ccmsService; 
 
-	// curl -X GET -i http://localhost:8080/ccms/api/catalog
-	@RequestMapping(value = "catalog", method = RequestMethod.GET)
+	// curl -X GET -i http://localhost:8080/ccms/api/catalog/{username}/{password}
+	@RequestMapping(value = "/catalog/{username}/{password}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
-	public Catalog catalog() {
+	public Catalog catalog(@PathVariable String username, @PathVariable String password) {
+		Authentication auth = new UsernamePasswordAuthenticationToken(username, password);
+		SecurityContextHolder.getContext().setAuthentication(auth);
 		return new Catalog(ccmsService.getAllCatalogItems());
 	}
 	
-	// curl -X GET -i http://localhost:8080/ccms/api/ci/{vAppName}
-	@RequestMapping(value = "ci/{vAppName}", method = RequestMethod.GET)
+	// curl -X GET -i http://localhost:8080/ccms/api/ci/{vAppName}/{username}/{password}
+	@RequestMapping(value = "ci/{vAppName}/{username}/{password}", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	@ResponseBody
-	public CatalogItem getCI(@PathVariable String vAppName) {
+	public CatalogItem getCI(@PathVariable String vAppName, @PathVariable String username, @PathVariable String password) {
+		Authentication auth = new UsernamePasswordAuthenticationToken(username, password);
+		SecurityContextHolder.getContext().setAuthentication(auth);
+		
 		CatalogItem ci = ccmsService.get(vAppName);
 		if (ci == null)
 			throw new NotFoundException();
 		return ci;
 	}
-
+		
 	// curl -X PUT -i http://localhost:8080/ccms/api/deploy/{vAppName}
-	@RequestMapping(value = "deploy/{vAppName}", method = RequestMethod.PUT)
+	@RequestMapping(value = "deploy/{vAppName}/{username}/{password}", method = RequestMethod.PUT)
 	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void update(@PathVariable String vAppName) {
+	public void update(@PathVariable String vAppName, @PathVariable String username, @PathVariable String password) {
 		try {
 			ccmsService.deploy(vAppName);
 		}
